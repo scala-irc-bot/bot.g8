8fb628902290c541cfb67a4be933b53ec4a70173
mtgto <hogerappa@gmail.com>
2012-12-15 12:23:43 +0900

    fix for scala-irc-bot 0.2.0-SNAPSHOT.

--- src/main/g8/src/main/scala/$organization__packaged$/$name__Camel$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$name__Camel$.scala
@@ -4,7 +4,7 @@ import net.mtgto.irc.{Bot, Client}
 import net.mtgto.irc.event._
 
 class $name;format="Camel"$ extends Bot {
-  override def onMessage(message: Message) = {
-    Client.sendNotice(message.channel, message.text)
+  override def onMessage(client: Client, message: Message) = {
+    client.sendNotice(message.channel, message.text)
   }
 }
